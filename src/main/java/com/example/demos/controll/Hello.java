adc0d07bcab16429a3a442b084823fa6e3bf8229
gaoxiaohui1 <648282572@qq.com>
2017-11-08 18:34:46 +0800

    third commit

--- src/main/java/com/example/demos/controll/Hello.java
+++ src/main/java/com/example/demos/controll/Hello.java
@@ -3,8 +3,11 @@ package com.example.demos.controll;
 
 import com.example.demos.model.User;
 import com.example.demos.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +22,12 @@ public class Hello {
     @Autowired
     private UserService us;
 
-    @GetMapping(value = "/s")
-    public User search() {
+
+    @ApiOperation(value = "获取单个User",notes = "获取单个User的JSON数据")
+    @ApiImplicitParam(name = "id",value = "用户id",required = true,dataType = "int")
+    @GetMapping(value = "/s/{id}")
+    public User search(@PathVariable(value = "id") Integer id) {
+        System.out.println(id);
         return us.getUser();
     }
 
